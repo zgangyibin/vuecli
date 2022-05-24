51b35d50cf0893c264a50dfcd1d156a23da54e84
zgangyibin <3112891950@qq.com>
2022-05-24 20:15:26 +0800

    name属性

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,19 @@
     <move></move>
     <hr>
     <tick></tick>
+    
+    <ComNameHaha></ComNameHaha>
   </div>
 </template>
 
 <script>
+import Com from './components/Com.vue'
 import More from './components/More.vue'
 import Move from './components/Move.vue'
 import Tick from './components/Tick.vue'
   export default {
-  components: { More, Move, Tick },
+    // 对象里的key是变量的话，要用[]属性名表达式
+  components: { More, Move, Tick, [Com.name]:Com},
     
   }
 </script>
---
+++ src/components/Com.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        我是一个Com组件
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"ComNameHaha",//注册时可以定义自己名字
+
+    }
+</script>
+
+<style scoped>
+
+</style>
